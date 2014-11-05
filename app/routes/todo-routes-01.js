8b5630e65f28922bd9ec45a4b779503ba70d9702
Michael Uranaka <michael_uranaka@datahouse.com>
2014-11-04 16:14:55 -1000

    Changing module.exports to exports.

--- app/routes/todo-routes-01.js
+++ app/routes/todo-routes-01.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 var Todo = mongoose.model('Todo');
 
-module.exports.create = function(req, res){
+exports.create = function(req, res){
   var todo = new Todo(req.body);
   todo.save(function(err, result){
     res.json(result) 
   });
-}
+};
--- app/routes/todo-routes-02.js
+++ app/routes/todo-routes-02.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var Todo = mongoose.model('Todo');
 
-module.exports.create = function(req, res){
+exports.create = function(req, res){
   var todo = new Todo(req.body);
   todo.save(function(err, result){
     res.json(result) 
   });
 }
 
-module.exports.list = function(req, res){
+exports.list = function(req, res){
   Todo.find({}, function(err, results){
     res.json(results);
   });
-}
+};

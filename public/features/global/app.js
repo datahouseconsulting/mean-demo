ad67567ab942328fde2304415b30a25a420b244e
Daniel Kong <daniel_kong@datahouse.com>
2014-11-06 15:22:35 -1000

    basic working angularjs app with an app page

---
+++ public/features/global/app.js
@@ -0,0 +1,18 @@
+var doeApp = angular.module('doeApp', ['ngResource', 'ngRoute']);
+
+
+// setup routes
+doeApp.config(function ($routeProvider) {
+
+  // Here is where we setup which location uri will serve which page, and also which controllers to associate with which page
+  $routeProvider.
+//    when('/login', {                                    controller: LoginController,                          templateUrl: 'features/login/login.html' }).
+    when('/home', {                                     controller: HomeController,          templateUrl: 'features/home/home.html' }).
+//    when('/user', {                                     controller: TestController,                           templateUrl: 'features/test/test.html' }).
+    when('/application', {                              controller: ApplicationController,   templateUrl: 'features/application/application.html' }).
+    otherwise({ redirectTo: '/home' });
+
+});
+
+
+
